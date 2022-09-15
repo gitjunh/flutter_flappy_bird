af394215f749b2bd1f862b84f35198ebe3608abc
secretary.py <secretary.py@gmail.com>
2022-09-16 00:54:38 +0900

    add a bird

---
+++ flappy_bird/lib/bird.dart
@@ -0,0 +1,22 @@
+import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
+
+class Bird extends SpriteAnimationComponent with HasGameRef {
+  final double _animationSpeed = .15;
+  late final SpriteAnimation _idleAnimation;
+  Bird() : super(size: Vector2(52, 36.7));
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    await _loadAnimations().then((void value) => {animation = _idleAnimation});
+  }
+
+  Future<void> _loadAnimations() async {
+    final SpriteSheet spriteSheet = SpriteSheet.fromColumnsAndRows(
+        image: await gameRef.images.load('bird.png'), columns: 3, rows: 1);
+
+    _idleAnimation = spriteSheet.createAnimation(
+        row: 0, stepTime: _animationSpeed, from: 0, to: 2);
+  }
+}
--- flappy_bird/lib/flappy_bird_game.dart
+++ flappy_bird/lib/flappy_bird_game.dart
@@ -1,12 +1,15 @@
 import 'package:flame/game.dart';
+import 'package:flappy_bird/bird.dart';
 import 'package:flappy_bird/bird_world.dart';
 
 class FlappyBirdGame extends FlameGame {
   final BirdWorld _birdWorld = BirdWorld();
+  final Bird _bird = Bird();
 
   @override
   Future<void> onLoad() async {
-    super.onLoad();
+    await super.onLoad();
     await add(_birdWorld);
+    await add(_bird);
   }
 }
