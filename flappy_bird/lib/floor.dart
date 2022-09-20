6148c0ce31663e4dd34b0df199464b670c94edaa
secretary.py <secretary.py@gmail.com>
2022-09-20 22:33:27 +0900

    move floor

--- flappy_bird/lib/floor.dart
+++ flappy_bird/lib/floor.dart
@@ -3,34 +3,25 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 
 class Floor extends SpriteComponent with HasGameRef {
-  late double xSize;
-  late double ySize;
-  late double xPos;
+  late double xSize = 336;
+  late double ySize = gameRef.size.x * 2 / 168 * 56;
+  late double xPos = 0;
   @override
   Future<void> onLoad() async {
     super.onLoad();
     sprite = await gameRef.loadSprite('floor.png');
     size = gameRef.size;
 
-    // xSize = gameRef.size.x * 2;
-    // ySize = xSize / 168 * 56;
-    // position = Vector2(0, gameRef.size.y - ySize * 0.22);
-    // size = Vector2(xSize, ySize);
+    xSize = gameRef.size.x * 2;
+    ySize = xSize / 168 * 56;
+    position = Vector2(0, gameRef.size.y - ySize * 0.22);
+    size = Vector2(xSize, ySize);
   }
 
   @override
   void update(double dt) {
     xPos -= dt * 50;
-    if (xPos.abs() >= gameRef.size.x) xPos = 0;
-  }
-
-  @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-    xSize = gameRef.size.x * 2;
-    ySize = xSize / 168 * 56;
     position = Vector2(xPos, gameRef.size.y - ySize * 0.22);
-    size = Vector2(xSize, ySize);
-    sprite!.render(canvas);
+    if (xPos.abs() >= gameRef.size.x) xPos = 0;
   }
 }
